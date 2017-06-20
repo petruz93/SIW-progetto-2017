c8dfbc71bb2173c20e1bba02aca05da1c013d19f
Pargolo <marcoperoni_@hotmail.it>
2017-06-20 17:31:01 +0200

    [ALL] Risolti bug.

---
+++ siw-progetto2017/src/main/java/it/uniroma3/galleria/utility/InputOutputHandler.java
@@ -0,0 +1,23 @@
+package it.uniroma3.galleria.utility;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import it.uniroma3.galleria.model.Opera;
+
+public class InputOutputHandler {
+
+	/*
+	public static String saveImageOpera(File file, Opera opera) {
+
+		BufferedImage image = new BufferedImage(file.set);
+		try {
+			ImageIO.write(image, "png", file);
+		} catch(IOException e) {
+			System.out.println("Write error for " + file.getPath() + ": " + e.getMessage());
+		}
+
+	}
+	*/
+
+}
